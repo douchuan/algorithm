0e088debc318e10452c1f0166c8bc303fd6dbf2a
Dou Chuan <1843657913@qq.com>
2021-05-08 21:52:41 +0800

    impl morris traverse

--- src/tree/binary/traverse.rs
+++ src/tree/binary/traverse.rs
@@ -90,7 +90,29 @@ pub struct LevelOrderVisitor2;
 /// ]
 pub struct ZigzagOrderVisitor;
 
+/// Two elements of a binary search tree (BST) are swapped by mistake.
+/// Recover the tree without changing its structure.
+/// Note: A solution using O(n) space is pretty straight forward.
+/// Could you devise a constant space solution?
+///
+/// 示例 1：
+/// 输入：root = [1,3,null,null,2]
+/// 输出：[3,1,null,null,2]
+/// 解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+///
+/// 示例 2：
+/// 输入：root = [3,1,4,null,null,2]
+/// 输出：[2,1,4,null,null,3]
+/// 解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。
+pub struct RecoverBinarySearchTree;
+
+/// Given two binary trees, write a function to check if they are equal or not.
+/// Two binary trees are considered equal if they are structurally identical and
+/// the nodes have the same value.
+pub struct SameTree;
+
 impl PreOrderVisitor {
+    /// 时间复杂度 O(n), 空间复杂度 O(n)
     fn iterate(tree: &Tree) -> Vec<usize> {
         let mut results = vec![];
         let mut stack = vec![];
@@ -110,6 +132,65 @@ impl PreOrderVisitor {
         results
     }
 
+    /// 时间复杂度 O(n), 空间复杂度 O(1)
+    ///
+    /// 大概思路：当一个node有left subtree时，需要遍历left subtree
+    /// 的各节点，完成left subtree的遍历，需要回溯到node，这个回
+    /// 溯指针记录在left_child.right中
+    ///
+    /// 点评：利用tree本身的node记录回溯指针（避免用栈记录回溯），
+    /// 使得空间复杂度由 O(n) => O(1)
+    fn morris(tree: &mut Tree) -> Vec<usize> {
+        let mut results = vec![];
+        let mut cur = tree.root;
+
+        while let Some(p) = cur {
+            let node = tree.node_at(p).expect("invalid cur node");
+            match node.left {
+                Some(left) => {
+                    let mut record = left;
+
+                    //traverse right subtree, find前驱node
+                    loop {
+                        let record_node = tree.node_at(record).expect("invalid record node");
+                        match record_node.right {
+                            Some(r) if r != p => record = r,
+                            _ => break,
+                        }
+                    }
+
+                    let record_node = tree.node_at(record).expect("invalid record node");
+                    match record_node.right {
+                        Some(r) => {
+                            //已线索化
+                            cur = node.right;
+                            let record_node =
+                                tree.node_at_mut(record).expect("invalid record node");
+                            record_node.right = None;
+                        }
+                        None => {
+                            results.push(node.value);
+
+                            //未线索化
+                            let record_node =
+                                tree.node_at_mut(record).expect("invalid record node");
+                            record_node.right = cur;
+                            cur = Some(left);
+                        }
+                    }
+                }
+                None => {
+                    results.push(node.value);
+                    //无left subtree, 直接跨到right subtree
+                    cur = node.right;
+                }
+            }
+        }
+
+        results
+    }
+
+    /// 时间复杂度 O(n), 空间复杂度 O(n)
     fn recursive(tree: &Tree) -> Vec<usize> {
         let mut results = vec![];
         fn visitor(tree: &Tree, p: Option<TreeIndex>, results: &mut Vec<usize>) {
@@ -404,34 +485,67 @@ impl ZigzagOrderVisitor {
 
 #[test]
 fn t_preorder_iter() {
-    let nodes = vec!["1", "#", "2", "3"];
-    let tree = construct::new_tree(&nodes);
-    let r = PreOrderVisitor::iterate(&tree);
-    assert_eq!(vec![1, 2, 3], r);
+    for (t, expect) in preorder_test_data() {
+        let mut tree = construct::new_tree(&t);
+        let r = PreOrderVisitor::iterate(&tree);
+        assert_eq!(
+            expect, r,
+            "tree = {:?}, expect = {:?}, r = {:?}",
+            t, expect, r
+        );
+    }
 }
 
 #[test]
-fn t_inorder_iter() {
-    let nodes = vec!["1", "#", "2", "3"];
-    let tree = construct::new_tree(&nodes);
-    let r = InOrderVisitor::iterate(&tree);
-    assert_eq!(vec![1, 3, 2], r);
+fn t_preorder_morris() {
+    for (t, expect) in preorder_test_data() {
+        let mut tree = construct::new_tree(&t);
+        let r = PreOrderVisitor::morris(&mut tree);
+        assert_eq!(
+            expect, r,
+            "tree = {:?}, expect = {:?}, r = {:?}",
+            t, expect, r
+        );
+    }
 }
 
 #[test]
 fn t_preorder_recursive() {
-    let nodes = vec!["1", "#", "2", "3"];
-    let tree = construct::new_tree(&nodes);
-    let r = PreOrderVisitor::recursive(&tree);
-    assert_eq!(vec![1, 2, 3], r);
+    for (t, expect) in preorder_test_data() {
+        let mut tree = construct::new_tree(&t);
+        let r = PreOrderVisitor::recursive(&mut tree);
+        assert_eq!(
+            expect, r,
+            "tree = {:?}, expect = {:?}, r = {:?}",
+            t, expect, r
+        );
+    }
+}
+
+#[test]
+fn t_inorder_iter() {
+    for (t, expect) in inorder_test_data() {
+        let tree = construct::new_tree(&t);
+        let r = InOrderVisitor::iterate(&tree);
+        assert_eq!(
+            expect, r,
+            "tree = {:?}, expect = {:?}, r = {:?}",
+            t, expect, r
+        );
+    }
 }
 
 #[test]
 fn t_inorder_recursive() {
-    let nodes = vec!["1", "#", "2", "3"];
-    let tree = construct::new_tree(&nodes);
-    let r = InOrderVisitor::recursive(&tree);
-    assert_eq!(vec![1, 3, 2], r);
+    for (t, expect) in inorder_test_data() {
+        let tree = construct::new_tree(&t);
+        let r = InOrderVisitor::recursive(&tree);
+        assert_eq!(
+            expect, r,
+            "tree = {:?}, expect = {:?}, r = {:?}",
+            t, expect, r
+        );
+    }
 }
 
 #[test]
@@ -496,4 +610,40 @@ fn t_levelorder_zigzag_traverse() {
     let tree = construct::new_tree(&nodes);
     let r = ZigzagOrderVisitor::recursive(&tree);
     assert_eq!(vec![vec![3], vec![20, 9], vec![15, 7]], r);
+}
+
+fn preorder_test_data() -> Vec<(Vec<&'static str>, Vec<usize>)> {
+    vec![
+        (vec!["1", "#", "2", "3"], vec![1, 2, 3]),
+        (
+            vec!["1", "2", "#", "3", "#", "#", "#", "4"],
+            vec![1, 2, 3, 4],
+        ),
+        (
+            vec!["1", "2", "#", "3", "#", "#", "#", "#", "4"],
+            vec![1, 2, 3, 4],
+        ),
+        (
+            vec!["1", "2", "#", "3", "4", "#", "#", "5"],
+            vec![1, 2, 3, 4, 5],
+        ),
+    ]
+}
+
+fn inorder_test_data() -> Vec<(Vec<&'static str>, Vec<usize>)> {
+    vec![
+        (vec!["1", "#", "2", "3"], vec![1, 3, 2]),
+        (
+            vec!["1", "2", "#", "3", "#", "#", "#", "4"],
+            vec![4, 3, 2, 1],
+        ),
+        (
+            vec!["1", "2", "#", "3", "#", "#", "#", "#", "4"],
+            vec![3, 4, 2, 1],
+        ),
+        (
+            vec!["1", "2", "#", "3", "4", "#", "#", "5"],
+            vec![5, 3, 2, 4, 1],
+        ),
+    ]
 }
