325963d08c411e6895fb04d9e1a0abd99a764b49
Dou Chuan <1843657913@qq.com>
2021-05-09 21:51:33 +0800

    fix ctor tree err

--- src/tree/binary/construct.rs
+++ src/tree/binary/construct.rs
@@ -34,8 +34,10 @@ pub fn new_tree(orig: &[&str]) -> Tree {
     let mut nt = NodeType::Root;
     let mut parent = None;
 
+    // println!("tokens = {:?}", tokens);
     while let Some(value) = tokens.pop_front() {
         let cur = tree.add_value(value);
+        // println!("parent = {:?}, cur = {}", parent, value);
 
         //待处理node入队
         records.push_back(cur);
@@ -49,12 +51,22 @@ pub fn new_tree(orig: &[&str]) -> Tree {
                 if let Some(parent) = parent {
                     let parent_node = tree.node_at_mut(parent).expect("invalid parent node");
                     parent_node.left = cur;
+                } else if let Some(cur) = cur {
+                    //无父接收，退货
+                    tokens.push_front(value);
+                    records.pop_back();
+                    tree.remove(cur);
                 }
             }
             NodeType::RightChild => {
                 if let Some(parent) = parent {
                     let parent_node = tree.node_at_mut(parent).expect("invalid parent node");
                     parent_node.right = cur;
+                } else if let Some(cur) = cur {
+                    //无父接收，退货
+                    tokens.push_front(value);
+                    records.pop_back();
+                    tree.remove(cur);
                 }
 
                 //parent的left&right child node构建完毕，取下一个
@@ -62,16 +74,6 @@ pub fn new_tree(orig: &[&str]) -> Tree {
                     parent = next;
                 }
             }
-        }
-
-        match (parent, cur) {
-            (None, Some(cur)) => {
-                //无父接收，退货
-                tokens.push_front(value);
-                records.pop_back();
-                tree.remove(cur);
-            }
-            _ => (),
         }
 
         nt = nt.next();
@@ -89,4 +91,30 @@ fn t_empty_tree() {
     let tree = new_tree(&["#"]);
     assert!(tree.arena.is_empty());
     assert!(tree.root.is_none());
+}
+
+#[test]
+fn t_tree() {
+    let tokens = vec!["1", "2", "#", "3", "4", "#", "#", "5"];
+    let tree = new_tree(&tokens);
+    let p0 = tree.root.expect("invalid p0");
+    let p0 = tree.node_at(p0).expect("invalid p0 node");
+    assert_eq!(p0.value, 1);
+    assert!(p0.right.is_none());
+    let p1 = p0.left.expect("invalid p0 left");
+    let p1 = tree.node_at(p1).expect("invalid p1 node");
+    assert_eq!(p1.value, 2);
+    //p2: '#'
+    let p3 = p1.left.expect("invalid p3");
+    let p3 = tree.node_at(p3).expect("invalid p3 node");
+    assert_eq!(p3.value, 3);
+    let p4 = p1.right.expect("invalid p4");
+    let p4 = tree.node_at(p4).expect("invalid p3 node");
+    assert_eq!(p4.value, 4);
+    //p5: '#'
+    //p6: '#'
+    let p7 = p3.left.expect("invalid p7");
+    let p7 = tree.node_at(p7).expect("invalid p7 node");
+    assert_eq!(p7.value, 5);
+    assert_eq!(tree.arena.len(), 5);
 }
