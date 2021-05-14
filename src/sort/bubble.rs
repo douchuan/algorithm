b28a68b3f85471828c7092aecd1d675b81867ddd
Dou Chuan <1843657913@qq.com>
2021-05-14 18:15:03 +0800

    impl bubble sort

---
+++ src/sort/bubble.rs
@@ -0,0 +1,68 @@
+//! 冒泡排序（Bubble Sort）
+//!
+//! 它重复地走访过要排序的元素列，依次比较两个相邻的元素，如果顺
+//! 序（如从大到小、首字母从Z到A）错误就把他们交换过来。走访元素
+//! 的工作是重复地进行直到没有相邻元素需要交换，也就是说该元素列
+//! 已经排序完成。
+//!
+//! 这个算法的名字由来是因为越小的元素会经由交换慢慢“浮”到数列的
+//! 顶端（升序或降序排列），就如同碳酸饮料中二氧化碳的气泡最终会
+//! 上浮到顶端一样，故名“冒泡排序”。
+//!
+//! 若初始状态有序，时间复杂度 O(n)
+//! 若初始状态反序，时间复杂度 O(n^2)
+//! 稳定排序算法
+
+use crate::sort::util;
+use test::Bencher;
+
+pub fn sort<T: Copy, F>(a: &mut [T], test: F)
+where
+    F: Fn(T, T) -> bool + Copy,
+{
+    let len = a.len();
+    for i in 0..len.saturating_sub(1) {
+        let mut swapped = false;
+
+        for j in 0..(len - 1 - i) {
+            if test(a[j], a[j + 1]) {
+                a.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+
+        if !swapped {
+            break;
+        }
+    }
+}
+
+#[test]
+fn t() {
+    let test = |x: i32, y: i32| x > y;
+    let data = util::plan_data();
+    for (t, expect) in data {
+        let mut tt = t.clone();
+        sort(&mut tt, test);
+        assert_eq!(tt, expect, "t = {:?}, expect = {:?}", t, expect);
+    }
+}
+
+#[bench]
+fn bench_small(b: &mut Bencher) {
+    b.iter(|| {
+        let mut numbs = [1, 2, 4, 8, 9, 9, 13, 17, 22];
+        let test = |x: i32, y: i32| x > y;
+        sort(&mut numbs, test);
+    });
+}
+
+#[bench]
+fn bench_large(b: &mut Bencher) {
+    let data = util::random_data(util::DATA_LEN);
+    b.iter(|| {
+        let mut numbs = data.clone();
+        let test = |x: i32, y: i32| x > y;
+        sort(&mut numbs, test);
+    });
+}
