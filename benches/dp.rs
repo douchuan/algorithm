ead4fef01bd6991d86ba5aa0a397560d58d07a32
Dou Chuan <1843657913@qq.com>
2021-05-25 19:33:27 +0800

    selection sort benches

--- benches/dp.rs
+++ benches/dp.rs
@@ -1,3 +1,6 @@
+#![feature(test)]
+extern crate test;
+
 use algo::dp;
 use test::Bencher;
 
--- benches/sort.rs
+++ benches/sort.rs
@@ -200,3 +200,37 @@ fn eq_data_quick(b: &mut Bencher) {
         sort::quick::sort(&mut numbs, f);
     });
 }
+
+#[bench]
+fn small_selection_sort(b: &mut Bencher) {
+    b.iter(|| {
+        let mut numbs = [1, 2, 4, 8, 9, 9, 13, 17, 22];
+        sort::selection::sort(&mut numbs);
+    });
+}
+
+#[bench]
+fn small_selection_tree_selection(b: &mut Bencher) {
+    b.iter(|| {
+        let numbs = [1, 2, 4, 8, 9, 9, 13, 17, 22];
+        sort::tree_selection::sort_desc(&numbs);
+    });
+}
+
+#[bench]
+fn large_selection_sort(b: &mut Bencher) {
+    let data = util::random_data(util::DATA_LEN);
+    b.iter(|| {
+        let mut numbs = data.clone();
+        sort::selection::sort(&mut numbs);
+    });
+}
+
+#[bench]
+fn large_selection_tree_selection(b: &mut Bencher) {
+    let data = util::random_data(util::DATA_LEN);
+    b.iter(|| {
+        let numbs = data.clone();
+        sort::tree_selection::sort_desc(&numbs);
+    });
+}
