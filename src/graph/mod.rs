392465ba9c109a13940761ad7c0cf37343611a8f
Dou Chuan <1843657913@qq.com>
2021-07-08 21:20:18 +0800

    graph: more graph processing fn

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -40,6 +40,40 @@ impl Graph {
     pub fn adj(&self, v: i32) -> Vec<i32> {
         unimplemented!()
     }
+
+    /// compute the degree of v
+    pub fn degree(&self, v: i32) -> usize {
+        self.adj(v).len()
+    }
+
+    /// compute maximum degree
+    pub fn max_degree(&self) -> usize {
+        let mut max = 0;
+        for v in 0..self.V() {
+            max = std::cmp::max(max, self.degree(v as i32));
+        }
+        max
+    }
+
+    /// compute average degree
+    pub fn avg_degree(&self) -> f32 {
+        2.0 * self.E() as f32 / self.V() as f32
+    }
+
+    /// count self-loops
+    pub fn number_of_self_loops(&self) -> usize {
+        let mut count = 0;
+        for v in 0..self.V() {
+            for w in self.adj(v as i32) {
+                if v as i32 == w {
+                    count += 1;
+                }
+            }
+        }
+
+        // each edge counted twice
+        count / 2
+    }
 }
 
 impl ToString for Graph {
