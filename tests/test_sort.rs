93b215ba6f7ecb27f7a69bec5658476b57c76eed
Dou Chuan <1843657913@qq.com>
2021-08-21 23:05:19 +0800

    replace testcase by macro

--- tests/test_sort.rs
+++ tests/test_sort.rs
@@ -2,68 +2,67 @@
 
 use algo::sort;
 
-#[test]
-fn bubble() {
-    let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
-        sort::bubble::sort(t);
-        assert!(t.is_sorted());
-    });
+macro_rules! case {
+    // to test fn is "sort::$p::sort"
+    // testcase name is "fn $p()"
+    ($p: ident) => {
+        #[test]
+        fn $p() {
+            let mut data = sort::util::vec_data();
+            for t in data.iter_mut() {
+                sort::$p::sort(t);
+                assert!(t.is_sorted());
+            }
+        }
+    };
+    // to test fn is "sort::$p::$f"
+    // testcase name is "fn $f()"
+    ($p: ident, $f: ident) => {
+        #[test]
+        fn $f() {
+            let mut data = sort::util::vec_data();
+            for t in data.iter_mut() {
+                sort::$p::$f(t);
+                assert!(t.is_sorted());
+            }
+        }
+    };
 }
 
-#[test]
-fn insert() {
-    let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
-        sort::insert::sort(t);
-        assert!(t.is_sorted());
-    });
-}
-
-#[test]
-fn selection() {
-    let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
-        sort::selection::sort(t);
-        assert!(t.is_sorted());
-    });
-}
-
-#[test]
-fn selection_cocktail() {
-    let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
-        sort::selection::sort_cocktail(t);
-        assert!(t.is_sorted());
-    });
-}
+case!(bubble);
+case!(insert);
+case!(selection);
+case!(selection, sort_cocktail);
+case!(quick);
+case!(shell);
+case!(floyd);
 
 #[test]
 fn tournament_tree() {
     let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
+    for t in data.iter_mut() {
         let mut t = sort::tree_selection::sort_desc(t);
         t.reverse();
         assert!(t.is_sorted());
-    });
+    }
 }
 
 #[test]
 fn merge_v1() {
     let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
+    for t in data.iter_mut() {
         let t = sort::merge::v1::sort(t);
         assert!(t.is_sorted());
-    });
+    }
 }
 
 #[test]
 fn merge_v2() {
     let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
+    for t in data.iter_mut() {
         sort::merge::v2::sort(t);
         assert!(t.is_sorted());
-    });
+    }
 }
 
 #[test]
@@ -71,33 +70,6 @@ fn merge_v3() {
     let mut data = sort::util::vec_data();
     data.iter_mut().for_each(|t| {
         sort::merge::v3::sort(t);
-        assert!(t.is_sorted());
-    });
-}
-
-#[test]
-fn quick() {
-    let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
-        sort::quick::sort(t);
-        assert!(t.is_sorted());
-    });
-}
-
-#[test]
-fn shell() {
-    let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
-        sort::shell::sort(t);
-        assert!(t.is_sorted());
-    });
-}
-
-#[test]
-fn heap_sort_floyd() {
-    let mut data = sort::util::vec_data();
-    data.iter_mut().for_each(|t| {
-        sort::floyd::sort(t);
         assert!(t.is_sorted());
     });
 }
