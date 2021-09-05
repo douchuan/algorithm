96190c1e411afba69d54a9275d305c5eb2b33d74
Dou Chuan <1843657913@qq.com>
2021-09-05 18:45:47 +0800

    str: deprecated attr for brute force search

--- src/strings/brute_force.rs
+++ src/strings/brute_force.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 use crate::common;
 
+#[deprecated(note = "brute force search v1 used by benchmark, use KMP in production")]
 pub fn search1(pat: &str, txt: &str) -> Option<usize> {
     let M = pat.len();
     let N = txt.len();
@@ -22,6 +23,7 @@ pub fn search1(pat: &str, txt: &str) -> Option<usize> {
     None
 }
 
+#[deprecated(note = "brute force search v2 used by benchmark, use KMP in production")]
 pub fn search2(pat: &str, txt: &str) -> Option<usize> {
     let M = pat.len();
     let N = txt.len();
