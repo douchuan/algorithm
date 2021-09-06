252c0c669ffa21007a3384898b1b82408d5999ad
Dou Chuan <1843657913@qq.com>
2021-09-06 21:07:32 +0800

    reimpl build tree in level

--- src/tree/binary/builder/level.rs
+++ src/tree/binary/builder/level.rs
@@ -9,96 +9,100 @@
 use crate::tree::binary::builder::TreeBuilder;
 use crate::tree::binary::node::Node;
 use crate::tree::binary::tree::Tree;
-use std::collections::LinkedList;
+use std::ptr::NonNull;
 
 pub trait BuildTreeInLevel<K, V> {
     fn build_in_level(vec: &[&str]) -> Tree<K, V>;
 }
 
-impl<K, V> BuildTreeInLevel<K, V> for TreeBuilder
-where
-    K: std::str::FromStr,
-{
+impl<K: std::str::FromStr, V> BuildTreeInLevel<K, V> for TreeBuilder {
     fn build_in_level(vec: &[&str]) -> Tree<K, V> {
         build(vec)
     }
 }
 
-fn build<K, V>(vec: &[&str]) -> Tree<K, V>
-where
-    K: std::str::FromStr,
-{
-    let mut tokens: LinkedList<&str> = LinkedList::new();
-    tokens.extend(vec.iter());
-
+// ?? why K: std::str::FromStr
+fn build<K: std::str::FromStr, V>(vec: &[&str]) -> Tree<K, V> {
+    let tokens = expand_sharp(vec);
     let mut tree = Tree::default();
-    let mut records = LinkedList::new();
-    let mut nt = NodeType::Root;
-    let mut parent = None;
+    let mut tree_size = 0;
+    let mut aux: Vec<Option<NonNull<Node<K, V>>>> = Vec::new();
+    for (i, &token) in tokens.iter().enumerate() {
+        let node = token.parse().map(Node::new_key).ok();
+        aux.push(node);
 
-    // println!("tokens = {:?}", tokens);
-    while let Some(value) = tokens.pop_front() {
-        let cur = value.parse().ok().map(Node::new_key);
-        // println!("parent = {:?}, cur = {}", parent, value);
-
-        match (parent, cur) {
-            (None, Some(cur)) if nt != NodeType::Root => {
-                //无父接收，退货
-                tokens.push_front(value);
-                Node::release(cur);
-            }
-            //待处理node入队
-            _ => records.push_back(cur),
+        if node.is_some() {
+            tree_size += 1;
         }
 
-        match nt {
-            NodeType::Root => {
-                tree.root = cur;
-                parent = records.pop_front().unwrap();
-            }
-            NodeType::LeftChild => {
-                if let Some(mut parent) = parent {
-                    unsafe {
-                        parent.as_mut().left = cur;
-                    }
-                }
-            }
-            NodeType::RightChild => {
-                if let Some(mut parent) = parent {
-                    unsafe {
-                        parent.as_mut().right = cur;
-                    }
-                }
-
-                //parent的left&right child node构建完毕，取下一个
-                if let Some(next) = records.pop_front() {
-                    parent = next;
-                }
-            }
+        if i == 0 {
+            tree.root = node;
+            continue;
         }
 
-        nt = nt.next();
+        match node {
+            Some(mut node) => unsafe {
+                let parent_i = ((i + 1) >> 1) - 1;
+                let left_i = (parent_i << 1) + 1;
+                let mut parent_node = aux[parent_i].unwrap();
+                node.as_mut().parent = Some(parent_node);
+                if left_i == i {
+                    parent_node.as_mut().left = Some(node);
+                } else {
+                    parent_node.as_mut().right = Some(node);
+                }
+            },
+            None => {}
+        }
     }
 
-    // assert!(records.iter().all(|it| it.is_none()));
-    // assert!(tokens.is_empty());
+    tree.set_size(tree_size);
 
     tree
 }
 
-#[derive(Copy, Clone, PartialOrd, PartialEq)]
-enum NodeType {
-    Root,
-    LeftChild,
-    RightChild,
-}
+// "#" symbol as empty child
+fn expand_sharp<'a>(vec: &[&'a str]) -> Vec<&'a str> {
+    let mut results = Vec::new();
 
-impl NodeType {
-    fn next(self) -> Self {
-        match self {
-            NodeType::Root => Self::LeftChild,
-            NodeType::LeftChild => Self::RightChild,
-            NodeType::RightChild => Self::LeftChild,
+    for &v in vec.iter() {
+        // root
+        if results.is_empty() {
+            results.push(v);
+            continue;
+        }
+
+        // child
+        if v == "#" {
+            results.push(v);
+        } else {
+            // child
+            loop {
+                // new child idx
+                let idx = results.len();
+                let parent_idx = ((idx + 1) >> 1) - 1;
+                let parent = results[parent_idx];
+                if results[parent_idx] == "#" {
+                    // if parent is "#", push "#"
+                    results.push(parent);
+                } else {
+                    results.push(v);
+                    break;
+                }
+            }
         }
     }
+
+    results
+}
+
+#[test]
+fn t_expand_sharp() {
+    let vec = vec!["1", "#", "2", "3"];
+    let results = vec!["1", "#", "2", "#", "#", "3"];
+    assert_eq!(results, expand_sharp(vec.as_slice()));
+
+    let vec = vec!["1", "2", "#", "3", "4", "#", "#", "5"];
+    let results = vec!["1", "2", "#", "3", "4", "#", "#", "5"];
+    assert_eq!(results, expand_sharp(vec.as_slice()));
 }
