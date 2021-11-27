d67ae0290c7a3510837cf8082ae5d3ae7ae6c8f3
douchuan <1843657913@qq.com>
2021-11-27 12:25:43 +0800

    another sqrt method

--- src/math/mysqrt.rs
+++ src/math/mysqrt.rs
@@ -1,11 +1,11 @@
-pub fn sqrt_binary_search(x: f32) -> f32 {
+pub fn sqrt_binary_search(x: f64) -> f64 {
     assert!(x >= 0.0);
     let mut low = 0.0;
     let mut up = x;
     let mut last = 0.0;
     loop {
         let mid = (low + up) / 2.0;
-        if (mid - last).abs() <= f32::EPSILON {
+        if (mid - last).abs() <= f64::EPSILON {
             return mid;
         }
 
@@ -19,12 +19,12 @@ pub fn sqrt_binary_search(x: f32) -> f32 {
     }
 }
 
-pub fn sqrt_newton(x: f32) -> f32 {
+pub fn sqrt_newton(x: f64) -> f64 {
     assert!(x >= 0.0);
     if x == 0.0 {
         return x;
     }
-    if (x - 1.0).abs() <= f32::EPSILON {
+    if (x - 1.0).abs() <= f64::EPSILON {
         return 1.0;
     }
     let mut last = 0.0;
@@ -32,9 +32,15 @@ pub fn sqrt_newton(x: f32) -> f32 {
     loop {
         // iter_v = iter_v - (iter_v * iter_v - x) / (2.0 * iter_v);
         iter_v = (iter_v + x / iter_v) / 2.0; //由上式化简得来
-        if (iter_v - last).abs() <= f32::EPSILON {
+        if (iter_v - last).abs() <= f64::EPSILON {
             return iter_v;
         }
         last = iter_v;
     }
+}
+
+// exp(log(x) * 0.5)
+pub fn sqrt3(x: f64) -> f64 {
+    assert!(x >= 0.0);
+    (x.ln() * 0.5f64).exp()
 }
--- tests/test_math.rs
+++ tests/test_math.rs
@@ -1,24 +1,13 @@
 use algo::math;
 
 #[test]
-fn sqrt_binary_search() {
+fn sqrt() {
+    // let epsilon = f64::EPSILON;
     let epsilon = 0.01;
-    let valus: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+    let valus: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
     for v in valus {
         assert!((v.sqrt() - math::mysqrt::sqrt_binary_search(v)) <= epsilon);
-    }
-}
-
-#[test]
-fn sqrt_newton() {
-    let epsilon = 0.01;
-    let valus: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
-    for v in valus {
-        assert!(
-            (v.sqrt() - math::mysqrt::sqrt_newton(v)) <= epsilon,
-            "std sqrt = {}, sqrt_newton = {}",
-            v.sqrt(),
-            math::mysqrt::sqrt_newton(v)
-        );
+        assert!((v.sqrt() - math::mysqrt::sqrt_newton(v)) <= epsilon);
+        assert!((v.sqrt() - math::mysqrt::sqrt3(v)) <= epsilon);
     }
 }
